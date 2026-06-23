ece447d2571f05f4492b577f0956b1bb5f7a3008
RajSingh3402 <satyamsingh82844@gmail.com>
2026-06-23 15:09:59 +0530

    Hide login card but keep background glows

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -86,6 +86,10 @@ export default function LoginPage() {
       }} />
 
       {/* Login Card */}
+      {/* 
+        TO UNCOMMENT: Change `false && (` to `true && (` (or remove the wrapper) to show the login card again.
+      */}
+      {false && (
       <div style={{
         ...S.card,
         width: '420px',
@@ -197,6 +201,7 @@ export default function LoginPage() {
           </button>
         </form>
       </div>
+      )}
     </div>
   );
 }
